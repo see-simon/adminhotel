034195b51f7fabe2da6157313c314db0251faa90
see-simon <60693190+see-simon@users.noreply.github.com>
2022-02-08 02:21:39 +0200

    changes

---
+++ src/components/methods.jsx
@@ -0,0 +1,23 @@
+import firebase from "firebase";
+const auth = firebase.app.auth();
+
+const db = firebase.ref('/users')
+const _db = firebase.ref('/users')
+
+
+class Users {
+
+signUp ( email, password){
+    createu
+};
+
+  login(Email, password){
+
+
+  }
+
+
+}
+
+
+
